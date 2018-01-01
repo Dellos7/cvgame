062baac1b1303d5a4512b1c723192ca9e2fdc728
David <david.lopez.castellote@gmail.com>
2018-01-01 12:16:10 +0100

    improved touch with enemies
    improved touch with enemies

--- cvgame.js
+++ cvgame.js
@@ -30,7 +30,21 @@ var cvGameData = {
     'years': [
         {
             'year': '1994',
-            'difficulty': 0
+            'difficulty': 100,
+            'achievements': [
+                {
+                    'name': 'Primaria',
+                    'month': 2
+                },
+                {
+                    'name': 'Primaria',
+                    'month': 3
+                },
+                {
+                    'name': 'Secundaria',
+                    'month': 10
+                }
+            ]
         },
         {
             'year': '1995',
@@ -62,13 +76,16 @@ var pxPerYearWithSpeed = pxPerYear / gameSpeed;
 var months = [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 
 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ];
 var numberOfMonthsInYear = months.length;
-var pxPerMonth = pxPerYearWithSpeed / numberOfMonthsInYear;
+//var pxPerMonth = pxPerYearWithSpeed / numberOfMonthsInYear;
+var pxPerMonth;
 
 var enemieRespawnFixedTimeout = 5000 / gameSpeed;
 var enemieRespawnRandom = Math.random();
 
 var currentEnemies;
+var achievements;
 
+//Set up variables for initialization of the game
 var setUpGame = function() {
     canvas = $('#cvGame')[0];
     context = canvas.getContext('2d');
@@ -76,14 +93,38 @@ var setUpGame = function() {
     yPlayer = canvas.height - PLAYER_HEIGHT_DEFAULT - 5;
     timelineX = 0;
     currentEnemies = [];
+    achievements = [];
+
+    pxPerMonth = canvas.width; //Each month fills the screen
 
     yPlayerDefault = yPlayer;
     x_debugline2 = canvas.width - X_DEBUGLINE1*3;
     keyboardEvents();
     runGame();
-    //looseEnemies();
+    tests();
 };
 
+var tests = function() {
+    var enemie = {
+        x: xPlayer,
+        y: yPlayer - (ENEMIE_HEIGHT_DEFAULT+5)
+    };
+    console.log('YPLAYER: ' + yPlayer);
+    console.log('XPLAYER: ' + xPlayer);
+    console.log('YENEMIE:' + enemie.y);
+    console.log('XENEMIE: ' + enemie.x);
+    console.log('player height default: ' + PLAYER_HEIGHT_DEFAULT);
+    console.log('player width default: ' + PLAYER_WIDTH_DEFAULT);
+    console.log('enemie height default: ' + ENEMIE_HEIGHT_DEFAULT);
+    console.log('enemie width default: ' + ENEMIE_WIDHT_DEFAULT);
+    console.log(enemieTouchedPlayer(enemie));
+    console.log('1x: ' + (xPlayer + PLAYER_WIDTH_DEFAULT/2) + ';>; ' + (enemie.x - ENEMIE_WIDHT_DEFAULT/2));
+    console.log('2x: ' + (xPlayer - (PLAYER_WIDTH_DEFAULT/2-1)) + ';<; ' + (enemie.x + (ENEMIE_WIDHT_DEFAULT/2-1)) );
+    console.log('3y: ' + (yPlayer - (PLAYER_HEIGHT_DEFAULT/2-1)) + ';<; ' + (enemie.y + (ENEMIE_HEIGHT_DEFAULT/2-1)));
+    console.log('4y: ' + (yPlayer + (PLAYER_HEIGHT_DEFAULT/2-1)) + ';>; ' + (enemie.y - (ENEMIE_HEIGHT_DEFAULT/2-1)));
+};
+
+//Reset game variables to start again
 var resetGame = function() {
     xPlayer = XPLAYER_DEFAULT;
     yPlayer = canvas.height - PLAYER_HEIGHT_DEFAULT - 5;
@@ -94,6 +135,7 @@ var resetGame = function() {
     currentMonth = undefined;
 };
 
+//This function gets executed each frame with the 'requestAnimationFrame' call
 var runGame = function() {
     requestAnimationFrame( runGame );
     resetCanvas();
@@ -107,12 +149,14 @@ var draw = function() {
     game();
 };
 
+//Game handling
 var game = function() {
     if( !gameOver ) {
         handlePlayerJump();
         handlePlayerDuck();
         readGameData();
         looseEnemies();
+        looseAchievements();
     }
 };
 
@@ -134,6 +178,7 @@ var initializeGameDataVariables = function() {
     }
 };
 
+//This function will handle the pass of the time simulation
 var passOfTheTime = function() {
     var currentYearAndMonth = calcCurrentYearAndMonth();
     currentYearNumber = currentYearAndMonth.year;
@@ -147,11 +192,17 @@ var setUpCurrentDifficulty = function() {
     currentDifficulty = currentYear.difficulty;
 };
 
+//Calculates current year and month from the timelineX variable
+//Depends on the pixels that we've set up per month of year
 var calcCurrentYearAndMonth = function() {
+    //The timelineX variable is usually negative, as the player moves in the time
+    //Starts in 0 -> -10, -20, -30... so we can know how much the player has proceeded
     var positiveTimelineX = (timelineX < 0 ? timelineX * (-1) : timelineX);
     if( positiveTimelineX > 0 ) {
+        //Ex. starting point it's 1994, and we are in Feb. 1995 --> would be the month 14
         var numberOfMonthWithoutYear = Math.ceil( positiveTimelineX / pxPerMonth );
         var numberOfYear = Math.ceil( numberOfMonthWithoutYear / numberOfMonthsInYear );
+        //Number of the month in the year
         var numberOfMonth = (numberOfMonthWithoutYear % numberOfMonthsInYear != 0 ? numberOfMonthWithoutYear % numberOfMonthsInYear : 12);
     }
     else {
@@ -172,6 +223,11 @@ var setUpCurrentMonth = function(monthNumber) {
     currentMonth = months[monthNumber - 1];
 };
 
+var getCurrentMonthNumber = function() {
+    return months.indexOf(currentMonth);
+};
+
+//Draws the current year and month in the canvas UI
 var setYearAndMonthInUI = function(year, month) {
     context.beginPath();
 	context.font = 'bold 10pt Arial'
@@ -179,6 +235,7 @@ var setYearAndMonthInUI = function(year, month) {
     context.fillText( month + ', ' + year , 10, 15);
 };
 
+//Loose the enemies in the game
 var looseEnemies = function() {
     if( shouldLooseNewEnemie() ) {
         looseNewEnemie();
@@ -186,15 +243,19 @@ var looseEnemies = function() {
     moveCurrentEnemies();
 };
 
+//It decides if an enemie should be loosed each frame of the game
+//Depends on the difficulty of the current year
 var shouldLooseNewEnemie = function() {
     var mRandom = Math.random();
     var looseNewEnemieRandom = Math.random();
-    currentDifficulty = 10; //REMOVE
+    //currentDifficulty = 10; //REMOVE
     //Generating a range that depends on the difficulty of the year
-    var range = 1/(100*((10-currentDifficulty)+1));
+    //var range = 1/(100*((10-currentDifficulty)+1));
+    var range = 1/(10*((100-currentDifficulty)+1));
     return looseNewEnemieRandom >= (mRandom-range) && (mRandom+range) >= looseNewEnemieRandom;
 };
 
+//Looses a new enemie
 var looseNewEnemie = function() {
     var lNewEnemie = true;
     //Do not allow to loose enemies too close
@@ -205,6 +266,7 @@ var looseNewEnemie = function() {
         }
     }
 
+    //Draws a new enemie and pushes it to the enemies array
     if( lNewEnemie ) {
         context.beginPath();
         context.fillStyle = "blue";
@@ -218,17 +280,40 @@ var looseNewEnemie = function() {
     }
 };
 
-var enemieTouchedPlayer = function(theEnemie) {
-    var eTouchedP = (xPlayer + PLAYER_WIDTH_DEFAULT/2) > (theEnemie.x - ENEMIE_WIDHT_DEFAULT/2)
+//Calculates if the enemie touched the player
+/*var enemieTouchedPlayer = function(theEnemie) {
+    var eTouchedP = 
+    //Enemie is at the right
+    (xPlayer + PLAYER_WIDTH_DEFAULT/2) > (theEnemie.x - ENEMIE_WIDHT_DEFAULT/2)
     &&
+    //Enemie is at the left
     (xPlayer - (PLAYER_WIDTH_DEFAULT/2-1)) < (theEnemie.x + (ENEMIE_WIDHT_DEFAULT/2-1)) 
-    && 
+    &&
+    //Enemie is at the top 
     (yPlayer - (PLAYER_HEIGHT_DEFAULT/2-1)) < (theEnemie.y + (ENEMIE_HEIGHT_DEFAULT/2-1))
     &&
+    //Enemie is at the bottom
     (yPlayer + (PLAYER_HEIGHT_DEFAULT/2-1)) > (theEnemie.y - (ENEMIE_HEIGHT_DEFAULT/2-1));
+    return eTouchedP;
+};*/
+var enemieTouchedPlayer = function(theEnemie) {
+    var eTouchedP = 
+    //Enemie is at the right
+    (xPlayer + PLAYER_WIDTH_DEFAULT) > (theEnemie.x)
+    &&
+    //Enemie is at the left
+    (xPlayer) < (theEnemie.x + (ENEMIE_WIDHT_DEFAULT)) 
+    &&
+    //Enemie is at the top 
+    (yPlayer) < (theEnemie.y + (ENEMIE_HEIGHT_DEFAULT))
+    &&
+    //Enemie is at the bottom
+    (yPlayer + (PLAYER_HEIGHT_DEFAULT)) > (theEnemie.y);
     return eTouchedP;
 };
 
+//Moves all the enemies in the screen in each frame of the game
+//If an enemie arrives to the end, removes it from the array of enemies
 var moveCurrentEnemies = function() {
     var toRemove = [];
     $.each( currentEnemies, function(index, enemie) {
@@ -241,6 +326,18 @@ var moveCurrentEnemies = function() {
             toRemove.push(index);
         }
         if( enemieTouchedPlayer(enemie) ) {
+            console.log('YPLAYER: ' + yPlayer);
+            console.log('XPLAYER: ' + xPlayer);
+            console.log('YENEMIE:' + enemie.y);
+            console.log('XENEMIE: ' + enemie.x);
+            console.log('player height default: ' + PLAYER_HEIGHT_DEFAULT);
+            console.log('player width default: ' + PLAYER_WIDTH_DEFAULT);
+            console.log('enemie height default: ' + ENEMIE_HEIGHT_DEFAULT);
+            console.log('enemie width default: ' + ENEMIE_WIDHT_DEFAULT);
+            console.log('1x: ' + (xPlayer + PLAYER_WIDTH_DEFAULT/2) + ';>; ' + (enemie.x - ENEMIE_WIDHT_DEFAULT/2));
+            console.log('2x: ' + (xPlayer - (PLAYER_WIDTH_DEFAULT/2-1)) + ';<; ' + (enemie.x + (ENEMIE_WIDHT_DEFAULT/2-1)) );
+            console.log('3y: ' + (yPlayer - (PLAYER_HEIGHT_DEFAULT/2-1)) + ';<; ' + (enemie.y + (ENEMIE_HEIGHT_DEFAULT/2-1)));
+            console.log('4y: ' + (yPlayer + (PLAYER_HEIGHT_DEFAULT/2-1)) + ';>; ' + (enemie.y - (ENEMIE_HEIGHT_DEFAULT/2-1)));
             gameOver = true;
         }
     });
@@ -248,6 +345,37 @@ var moveCurrentEnemies = function() {
     for(var i = 0; i < toRemove.length; i++) {
         currentEnemies.splice(toRemove[i], 1);
     }
+};
+
+var looseAchievements = function() {
+    if( currentYear && currentMonth ) {
+        var currentYearAchievements = currentYear.achievements;
+        var currentMonthNumber = getCurrentMonthNumber();
+        var thisMonthAchievements = [];
+        if( currentYearAchievements ) {
+            $.each( currentYearAchievements, function(index, achievement) {
+                if( achievement && achievement.month === currentMonthNumber && !achievement.loosed ) {
+                    thisMonthAchievements.push(achievement);
+                    achievement.loosed = true;
+                    achievement.x = canvas.width;
+                    achievement.y = yPlayerDefault;
+                    achievement.defaultTimelineX = timelineX;
+                }
+            });
+            achievements = achievements.concat(thisMonthAchievements);
+        }
+    }
+
+    moveAchievements();
+};
+
+var moveAchievements = function() {
+    $.each( achievements, function(index, achievement) { //X_PX_MOVE_LEFT_RIGHT
+        var xMove = timelineX - achievement.defaultTimelineX;
+        context.beginPath();
+        context.fillStyle = "orange";
+        context.fillRect( achievement.x + xMove, 50, 50, 50 );
+    });
 };
 
 //Should be kind of random
@@ -277,6 +405,25 @@ var drawPlayer = function() {
     context.beginPath();
     context.fillStyle = PLAYER_FILLCOLOR_DEFAULT;
     context.fillRect( xPlayer, yPlayer, PLAYER_WIDTH_DEFAULT, PLAYER_HEIGHT_DEFAULT );
+
+    /*context.beginPath();
+    context.moveTo(0, yPlayer);
+    context.lineTo(canvas.width, yPlayer);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(xPlayer, canvas.height);
+    context.lineTo(xPlayer, 0);
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = "green";
+    context.fillRect( xPlayer, yPlayer - (ENEMIE_HEIGHT_DEFAULT+5), ENEMIE_WIDHT_DEFAULT, ENEMIE_HEIGHT_DEFAULT );
+
+    context.beginPath();
+    context.moveTo(0, yPlayer - (ENEMIE_HEIGHT_DEFAULT+5));
+    context.lineTo(canvas.width, yPlayer - (ENEMIE_HEIGHT_DEFAULT+5));
+    context.stroke();*/
 };
 
 var handlePlayerJump = function() {
